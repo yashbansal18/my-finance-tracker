f9421c97d64fb61fe3fe085509e2e8081f7a590e
Yash Bansal <109197939+yashbansal18@users.noreply.github.com>
2026-05-03 21:34:46 +0530

    fix: apiUrl to include '/api' path

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://my-finance-tracker-u51i.onrender.com' // Replace with your Render URL after deploy
+  apiUrl: 'https://my-finance-tracker-u51i.onrender.com/api' // Replace with your Render URL after deploy
 };
